e2b62c42a0a0bbf564b82322cf25434783102eaa
Sandro1234 <sandro.vnib@gmail.com>
2023-01-30 14:17:13 +0700

    add SideBar.vue

---
+++ src/components/SideBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <Datepicker v-model="date"></Datepicker>
+</template>
+
+<script>
+import Datepicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+export default {
+  name: "SideBar",
+  components: { Datepicker },
+  data() {
+    return {
+      date: null,
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
